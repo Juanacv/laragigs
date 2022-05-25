249f29c67b99fff666dfd80209f5e7476bb99bc4
Juan Antonio <juan.acv@gmail.com>
2022-05-25 16:49:24 +0200

    primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Tutorial Laravel 9
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello ', function() {
+    return response('<h1>Hello world</h1>',200)->header('Content-type','text/plain');
+});
+
+Route::get('/posts/{id}', function ($id) {
+   return response('Posts '.$id); 
+})->where('id','[0-9]+');
+
+Route::get('/search', function(Request $req) {
+    return $req->name . ' ' . $req->city;
+});
